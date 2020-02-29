df93a1bb20bb786e2a81b4155c7d28d9d70b8f24
André <andre.hodapp@t-online.de>
2020-02-29 09:27:26 +0100

    SimpleBot jetzt fertig zum Verbinden

--- src/main/java/org/ase/fourwins/udp/server/Main.java
+++ src/main/java/org/ase/fourwins/udp/server/Main.java
@@ -8,9 +8,9 @@ import org.ase.fourwins.client.udphelper.UdpCommunicator;
 public class Main {
 
   public static void main(String[] args) throws IOException {
-    String serverHost = "localhost";
+    String serverHost = "192.168.1.136";
     int serverPort = 4446;
-    String botname = "BotName";
+    String botname = "Rudolf der Vernichter";
     System.out.printf("Connecting to %s:%d. Botname: %s\n", serverHost, serverPort, botname);
     UdpCommunicator communicator = new UdpCommunicator(serverHost, serverPort);
     MessageListener bot = new SimpleBot(botname, communicator.getMessageSender());
